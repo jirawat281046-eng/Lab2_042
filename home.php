e3e9c1a155bfe92a4aadbf74cabbbfc7c2651857
jirawat281046-eng <jirawat281046@gmail.com>
2026-01-13 12:09:04 +0700

    commit massage

--- home.php
+++ home.php
@@ -2,8 +2,8 @@
 	<table width="1238" height="530" border="0">
 		<tr>
 			<td width="23" rowspan="2">&nbsp;</td>
-			<td height="119" colspan="2" bgcolor="#006600">
-				<h1><center><font color="#FFFFFF">พรชัยอพาร์ตเมนท์</font></center></h1>
+			<td height="119" colspan="2" bgcolor="#661d00">
+				<h1><center><font color="#FFFFFF">จิรวัฒน์อพาร์ตเมนท์</font></center></h1>
 				<?php include "Date.php";?>
 			</td>
 			<td width="43" rowspan="2">&nbsp;</td>
@@ -20,7 +20,7 @@
 				<p>
 					เชิญชมห้องตัวอย่าง ได้ทุกวัน <a href=amit.php>ติดต่อสอบถาม</a>	
 			</td>
-			<td width="190" bgcolor="#FFFF66"><ul>
+			<td width="190" bgcolor="#390db3"><ul>
 				<li><a href="home.php">หน้าแรก</li>
 				<li><a href="room.php">ห้องพัก&ราคา</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
